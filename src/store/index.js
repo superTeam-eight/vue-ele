332fcf034dbbbd57540432cd58e527de888fd7ce
losped <losped@live.cn>
2019-01-18 17:30:54 +0800

    dev-init1

---
+++ src/store/index.js
@@ -0,0 +1,20 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+import getters from './getters'
+import actions from './actions'
+import mutations from './mutations'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  state: {
+
+  },
+  getters,
+  mutations,
+  actions,
+  modules: {
+    
+  }
+})
