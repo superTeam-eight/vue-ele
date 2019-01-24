a7624699757f0e9d01083d0560f2bca47ac7007c
mrgao <gxwj1536@163.com>
2019-01-24 10:49:22 +0800

    oreder惊天页面完成

---
+++ src/components/order/List.vue
@@ -0,0 +1,125 @@
+<template>
+    <div class="list">
+            <div class="top">
+                <div class="left">
+                    <img src="http://elm.cangdu.org/img/16836c78a1225354.jpg" alt="">
+                </div>
+                <div class="right">
+                    <div class="first">
+                        <div class="left">
+                            <p class="title">米多面多(须水店)<i class="cubeic-arrow"></i> </p>
+                            <p class="time">2019-01-12 18:13</p>
+                        </div>
+                        <div class="right">
+                            订单已送达
+                        </div>
+                    </div>
+                    <div class="second">
+                        <p class="name">头豆牛肉拌饭+花花牛酸奶</p>
+                        <p class="price">￥28.5</p>
+                    </div>
+                </div>
+            </div>
+            <div class="botton">
+                <button>再来一单</button>
+                <button>评价的金币</button>
+            </div>
+        </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+    @import  '../../style/common';
+    .list{
+            background-color: #fff;
+            padding: 20px 0 20px 20px;
+            margin-top:20px;  
+            .top{
+                display: flex;
+                border-bottom: 1px solid #ededed;
+                .left{
+                    margin-right: 20px;
+                    img{
+                        width: 64px;
+                        height: 64px;
+                    }
+                }
+                .right{
+                    padding-right:20px; 
+                    flex: 1;
+                    .first{
+                        display: flex;
+                        justify-content: space-between;
+                        border-bottom: 1px solid #ededed;
+                        .left{
+                            .title{
+                                font-size: 32px;
+                                color:$titleColor;
+                                height: 50px;
+                                line-height: 50px;
+                                font-weight: bold;
+                                width: 320px;
+                                white-space: nowrap;
+                                overflow: hidden;
+                                text-overflow: ellipsis;
+                            }
+                            .time{
+                                height: 50px;
+                                line-height: 50px;
+                                font-size: 24px;
+                                color: $fontColor;
+                            }
+                        }
+                        .right{
+                            width:100%;
+                            font-size: 26px;
+                            color:$titleColor;
+                            height: 50px;
+                            line-height: 50px;
+                            text-align: right;
+                        }
+                    }
+                    .second{
+                        display: flex;
+                        justify-content: space-between;
+                        padding-right: 20px;
+                        .name{
+                            font-size: 26px;
+                            color: #656565;
+                            width: 280px;
+                            white-space: nowrap;
+                            overflow: hidden;
+                            text-overflow: ellipsis;
+                            height: 80px;
+                            line-height: 80px;
+                        }
+                        .price{
+                            font-size: 28px;
+                            color: $titleColor;
+                            font-weight: bolder;
+                            height: 80px;
+                            line-height: 80px;
+                        }
+                    }
+                }
+            }  
+            .botton{
+                text-align: right;
+                button{
+                    font-size: 26px;
+                    color: $themeColor;
+                    padding: 10px;
+                    border-radius: 10px;
+                    border:2px solid $themeColor;
+                    background-color: #fff;
+                    outline: none;
+                    margin:20px 20px 20px 0;
+                }
+            }
+        }
+</style>
---
+++ src/components/order/NoLogin.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="box1">
+        <img src="https://fuss10.elemecdn.com/d/60/70008646170d1f654e926a2aaa3afpng.png" alt="">
+        <p>登录后就可产看订单</p>
+        <button>登录</button>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang='scss' scoped>
+    @import '../../style/common';
+    .box1{
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        margin-top:20%;
+        img{
+            display: block;
+            width: 50%;            
+        }
+        p{
+            font-size: 30px;
+            height: 100px;
+            line-height: 100px;
+            color: $fontColor;
+        }
+        button{
+            background-color: none;
+            outline:none;
+            font-size: 38px;
+            color: #fff;
+            background: #56d176;
+            width: 50%;
+            letter-spacing: 10px;
+            height: 100px;
+            text-align: center;
+            line-height: 100px;
+            border: none;
+            
+        }
+    }
+</style>
