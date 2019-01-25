d96b0de55974b463b0e8529b10d1093d33be18d6
bao2333 <2919293547@qq.com>
2019-01-25 10:47:55 +0800

    路由的修改

--- src/router/router.js
+++ src/router/router.js
@@ -26,55 +26,6 @@ export default new Router({
         {
           path: 'profile',
           component: () => import('../views/index/Profile'),
-        },
-        {
-          path: 'info',
-          component: () => import('../views/index/profile/Info')
-        },
-        {
-          path: 'setusername',
-          component: () => import('../views/index/profile/user/SetUser.vue')
-        },{
-          path: 'setphone',
-          component: () => import('../views/index/profile/user/SetPhone.vue')
-        },
-        {
-          path: 'setpassword',
-          component: () => import('../views/index/profile/user/SetPassword.vue')
-        },
-        {
-          path: 'benefit',
-          component: () => import('../views/index/profile/Benefit'),
-          children: [
-            {
-              path: 'hongbao',
-              component: () => import('../views/index/profile/benefit/HongBao.vue')
-            },
-            {
-              path: 'coupon',
-              component: () => import('../views/index/profile/benefit/Coupon.vue')
-            }
-          ]
-        },
-        {
-          path: '/serve',
-          component: () => import('../views/index/profile/serve/Server.vue')
-        },
-        {
-          path: '/vipcenter',
-          component: () => import('../views/index/profile/serve/VipCenter.vue')
-        },
-        {
-          path: '/serverinfo',
-          component: () => import('../views/index/profile/serve/ServerInfo.vue')
-        },
-        {
-          path: '/myaddress',
-          component: () => import('../views/index/profile/address/Add.vue')
-        },
-        {
-          path: '/edit',
-          component: () => import('../views/index/profile/address/Edit.vue')
         }
       ]
     },
@@ -98,6 +49,55 @@ export default new Router({
       // home页面搜索商品的页面
       path:'/search',
       component:() => import ('../views/search/Search')
+    },
+    {
+      path: '/info',
+      component: () => import('../views/index/profile/Info')
+    },
+    {
+      path: '/setusername',
+      component: () => import('../views/index/profile/user/SetUser.vue')
+    },{
+      path: '/setphone',
+      component: () => import('../views/index/profile/user/SetPhone.vue')
+    },
+    {
+      path: '/setpassword',
+      component: () => import('../views/index/profile/user/SetPassword.vue')
+    },
+    {
+      path: '/benefit',
+      component: () => import('../views/index/profile/Benefit'),
+      children: [
+        {
+          path: 'hongbao',
+          component: () => import('../views/index/profile/benefit/HongBao.vue')
+        },
+        {
+          path: 'coupon',
+          component: () => import('../views/index/profile/benefit/Coupon.vue')
+        }
+      ]
+    },
+    {
+      path: '/serve',
+      component: () => import('../views/index/profile/serve/Server.vue')
+    },
+    {
+      path: '/vipcenter',
+      component: () => import('../views/index/profile/serve/VipCenter.vue')
+    },
+    {
+      path: '/serverinfo',
+      component: () => import('../views/index/profile/serve/ServerInfo.vue')
+    },
+    {
+      path: '/myaddress',
+      component: () => import('../views/index/profile/address/Add.vue')
+    },
+    {
+      path: '/edit',
+      component: () => import('../views/index/profile/address/Edit.vue')
     }
   ],
   linkExactActiveClass: 'active'
